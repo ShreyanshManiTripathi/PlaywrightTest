a8b4ce00153cbe550d362c4c794c5c2a2ca19d04
ShreyanshManiTripathi <shreyanshManiTripathi8@example.com>
2025-03-31 23:05:08 +0530

    sixthDcommit

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://www.facebook.com');   
 await page.locator('//input[@id="email"]').fill('shreyansh12octob@gmail.com');
 await page.locator('//input[@id="pass"]').fill('STEPHEN#@#');
-await page.locator('//button[@name="login"]').click();
+await page.locator('//button[@name="login"]').click(); 
 
 await page.waitForTimeout(5000);
 //   // Expect a title "to contain" a substring.
